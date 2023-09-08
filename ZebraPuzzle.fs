8a4f9d31b9437a3a742c6529e63f7078e5da6dbf
Stephane Larocque <stefsharp@slarocque.ca>
2023-09-08 18:22:55 -0400

    Complete initial lists

--- ZebraPuzzle.fs
+++ ZebraPuzzle.fs
@@ -22,68 +22,109 @@ module ZebraPuzzle
 //
 // // Which of the residents drinks water? Who owns the zebra?
 //
-// let always v _ = v
-//
+let always v _ = v
+
 type HouseColor = Red | Green | Ivory | Yellow | Blue
 type Nationality = Englishman | Spaniard | Ukrainian | Norwegian | Japanese
 type Pet = Dog | Snails | Fox | Horse | Zebra
 type Drink = Coffee | Tea | Milk | OrangeJuice | Water
 type CigaretteBrand = OldGold | Kools | Parliaments | Chesterfields | LuckyStrike
-//
-// type HouseProperty =
-//     | Color of HouseColor
-//     | OwnerNationality of Nationality
-//     | OwnerPet of Pet
-//     | OwnerDrink of Drink
-//     | OwnerCigaretteBrand of CigaretteBrand
-//     | Pair of HouseProperty * HouseProperty
-//     | Unknown
-//     
-// type House = HouseProperty list
-// let initialHouse = List.init 5 (always Unknown)
-//
-// let ColorIndex, OwnerNationalityIndex, OwnerPetIndex, OwnerDrinkIndex, OwnerCigaretteBrandIndex = (0, 1, 2, 3, 4)
-//     
-// type PredicateResult = Undefined | Success | Failure
-// type Predicate =
-//     | SingleProperty of (HouseProperty -> PredicateResult)
-//     | AllHouses of (House list -> PredicateResult)
-//     | SingleHouse of (House -> PredicateResult)
-//     | PairHouses of (House -> House -> PredicateResult)
-//
-// let ``The Englishman lives in the red house`` =
-//     Pair (Color Red, OwnerNationality Englishman)
-//     
-// let ``Coffee is drunk in the green house`` =
-//     Pair (Color Green, OwnerDrink Coffee)
-//     
-// let ``Kools are smoked in the yellow house`` =
-//     Pair (Color Yellow, OwnerCigaretteBrand Kools)
-//     
-// let allColors = [
-//     ``The Englishman lives in the red house``
-//     ``Coffee is drunk in the green house``
-//     Color Ivory
-//     ``Kools are smoked in the yellow house``
-//     Color Blue
-// ]
-//
-// let ``The Spaniard owns the dog`` =
-//     Pair (OwnerNationality Spaniard, OwnerPet Dog)
-//     
-// let ``The Ukrainian drinks tea`` =
-//     Pair (OwnerNationality Ukrainian, OwnerDrink Tea)
-//     
-// let ``The Japanese smokes Parliaments`` =
-//     Pair (OwnerNationality Japanese, OwnerCigaretteBrand Parliaments)
-//
-// let allNationalities = [
-//     OwnerNationality Englishman
-//     ``The Spaniard owns the dog``
-//     ``The Ukrainian drinks tea``
-//     OwnerNationality Norwegian
-//     ``The Japanese smokes Parliaments``
-// ]
+
+type HouseProperty =
+    | Color of HouseColor
+    | OwnerNationality of Nationality
+    | OwnerPet of Pet
+    | OwnerDrink of Drink
+    | OwnerCigaretteBrand of CigaretteBrand
+    | Pair of HouseProperty * HouseProperty
+    | Unknown
+    
+type House = HouseProperty list
+let initialHouse = List.init 5 (always Unknown)
+
+let ``Milk is drunk in the middle house`` =
+    [ Unknown; Unknown; Unknown; OwnerDrink Milk; Unknown ]
+    
+let ``The Norwegian lives in the first house`` =
+    [ Unknown; OwnerNationality Norwegian; Unknown; Unknown; Unknown ]
+    
+let ``The Norwegian lives next to the blue house`` =
+    [ Color Blue; Unknown; Unknown; Unknown; Unknown ]
+    
+let initialHouses = [
+    ``The Norwegian lives in the first house``
+    ``The Norwegian lives next to the blue house``
+    ``Milk is drunk in the middle house``
+    initialHouse
+    initialHouse
+]
+
+let ColorIndex, OwnerNationalityIndex, OwnerPetIndex, OwnerDrinkIndex, OwnerCigaretteBrandIndex = (0, 1, 2, 3, 4)
+    
+type PredicateResult = Undefined | Success | Failure
+type Predicate =
+    | SingleProperty of (HouseProperty -> PredicateResult)
+    | AllHouses of (House list -> PredicateResult)
+    | SingleHouse of (House -> PredicateResult)
+    | PairHouses of (House -> House -> PredicateResult)
+
+let ``The Englishman lives in the red house`` =
+    Pair (Color Red, OwnerNationality Englishman)
+    
+let ``Coffee is drunk in the green house`` =
+    Pair (Color Green, OwnerDrink Coffee)
+    
+let ``Kools are smoked in the yellow house`` =
+    Pair (Color Yellow, OwnerCigaretteBrand Kools)
+    
+let allColors = [
+    ``The Englishman lives in the red house``
+    ``Coffee is drunk in the green house``
+    Color Ivory
+    ``Kools are smoked in the yellow house``
+]
+
+let ``The Spaniard owns the dog`` =
+    Pair (OwnerNationality Spaniard, OwnerPet Dog)
+    
+let ``The Ukrainian drinks tea`` =
+    Pair (OwnerNationality Ukrainian, OwnerDrink Tea)
+    
+let ``The Japanese smokes Parliaments`` =
+    Pair (OwnerNationality Japanese, OwnerCigaretteBrand Parliaments)
+
+let allNationalities = [
+    ``The Spaniard owns the dog``
+    ``The Ukrainian drinks tea``
+    ``The Japanese smokes Parliaments``
+]
+
+let allDrinks = [
+    OwnerDrink Water
+]
+
+let ``The Old Gold smoker owns snails`` =
+    Pair (OwnerCigaretteBrand OldGold, OwnerPet Snails)
+    
+let ``The Lucky Strike smoker drinks orange juice`` =
+    Pair (OwnerCigaretteBrand LuckyStrike, OwnerDrink OrangeJuice)
+
+let allCigarettes = [
+    ``The Old Gold smoker owns snails``
+    OwnerCigaretteBrand Chesterfields
+    ``The Lucky Strike smoker drinks orange juice``
+]
+
+let allPets = [
+    OwnerPet Fox
+    OwnerPet Horse
+    OwnerPet Zebra
+]
+
+let rec cartesian = function
+  | [] -> Seq.singleton []
+  | L::Ls -> cartesian Ls |> Seq.collect (fun C -> L |> Seq.map (fun x->x::C))
+  
 //
 // let permute list =
 //   let rec inserts e = function
@@ -151,29 +192,14 @@ type CigaretteBrand = OldGold | Kools | Parliaments | Chesterfields | LuckyStrik
 //
 //
 //
-// let ``The green house is immediately to the right of the ivory house`` =
-//     lockSiblingProperties (Color Ivory) (Color Green)
-//
-// let ``The Old Gold smoker owns snails`` =
-//     lockProperties (OwnerCigaretteBrand OldGold) (OwnerPet Snails)
-//
-// let ``Milk is drunk in the middle house`` =    
-//     lockProperties (OwnerDrink Milk) (Position Middle)
-//
-// let ``The Norwegian lives in the first house`` =
-//     lockProperties (OwnerNationality Norwegian) (Position First)
-//
-// let ``The man who smokes Chesterfields lives in the house next to the man with the fox`` =
-//     siblingProperties (OwnerCigaretteBrand Chesterfields) (OwnerPet Fox)
-//
-// let ``Kools are smoked in the house next to the house where the horse is kept`` =
-//     siblingProperties (OwnerPet Horse) (OwnerCigaretteBrand Kools)
-//
-// let ``The Lucky Strike smoker drinks orange juice`` =
-//     lockProperties (OwnerCigaretteBrand LuckyStrike) (OwnerDrink OrangeJuice)
-//
-// let ``The Norwegian lives next to the blue house`` =
-//     siblingProperties (OwnerNationality Norwegian) (Color Blue)
+let ``The green house is immediately to the right of the ivory house`` =
+    lockSiblingProperties (Color Ivory) (Color Green)
+
+let ``The man who smokes Chesterfields lives in the house next to the man with the fox`` =
+    siblingProperties (OwnerCigaretteBrand Chesterfields) (OwnerPet Fox)
+
+let ``Kools are smoked in the house next to the house where the horse is kept`` =
+    siblingProperties (OwnerPet Horse) (OwnerCigaretteBrand Kools)
 //
 // let statements = [
 //     ``The Englishman lives in the red house``
